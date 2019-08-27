fec1afb5575fafe957039eea3173069562d9c48d
godliness <1229727237@qq.com>
2019-08-27 17:09:42 +0800

    first commit

---
+++ module1/src/main/module/java/Module1SplashActivity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by godliness on 2019-08-27.
+ *
+ * @author godliness
+ */
+public final class Module1SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
