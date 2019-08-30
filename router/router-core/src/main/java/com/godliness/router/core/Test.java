9dfd3c2ce9d66eaefe12f9719f6926b4574394e3
godliness <1229727237@qq.com>
2019-08-30 10:52:56 +0800

    remove router package

--- router/router-core/src/main/java/com/godliness/router/core/Test.java
+++
@@ -1,9 +0,0 @@
-package com.godliness.router.core;
-
-/**
- * Created by godliness on 2019-08-29.
- *
- * @author godliness
- */
-public class Test {
-}
--- router/router-core/src/main/java/com/godliness/router/core/template/IRouteRoot.java
+++
@@ -1,9 +0,0 @@
-package com.godliness.router.core.template;
-
-/**
- * Created by godliness on 2019-08-29.
- *
- * @author godliness
- */
-public interface IRouteRoot {
-}
